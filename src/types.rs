db2bfd6f55b01c3513c8830b8572946b441ee714
Gods <reid@wolfbyte.llc>
2026-04-27 13:17:16 -0500

    use dedicated values field for custom component values

--- src/types.rs
+++ src/types.rs
@@ -40,8 +40,8 @@ pub struct ComponentEntry {
     pub namespace: Option<String>,
     #[serde(default)]
     pub helm_flags: Option<Vec<String>>,
-    #[serde(flatten)]
-    pub extra: serde_json::Map<String, serde_json::Value>,
+    #[serde(default)]
+    pub values: Option<serde_json::Value>,
 }
 
 impl ComponentEntry {
@@ -54,11 +54,14 @@ impl ComponentEntry {
     }
 
     pub fn to_data_values_yaml(&self) -> String {
-        let mut map = self.extra.clone();
-        map.insert("name".to_string(), serde_json::Value::String(self.name.clone()));
-        map.insert("id".to_string(), serde_json::Value::String(self.id().to_string()));
-        map.insert("namespace".to_string(), serde_json::Value::String(self.namespace().to_string()));
-        let wrapper = serde_json::json!({ "component": map });
+        let wrapper = serde_json::json!({
+            "component": {
+                "name": self.name,
+                "id": self.id(),
+                "namespace": self.namespace(),
+                "values": self.values.clone().unwrap_or(serde_json::json!({})),
+            }
+        });
         serde_yaml::to_string(&wrapper).unwrap_or_default()
     }
 }
